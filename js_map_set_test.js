d85d9a0442e310fc957f246061ae59613a067782
Elcid Tesoro <elcid@innovationlove.ph>
2021-04-20 23:51:21 +0800

    reference

---
+++ js_map_set_test.js
@@ -0,0 +1,82 @@
+const myArray = [1, 1, 2, 3, 3, 4, 4, 4, 4, 5];
+const mySet = new Set(myArray);
+const unique = [...mySet];
+// =====================================================
+const myArray = [1, 1, 2, 3, 4, 5];
+const mySet = new Set(myArray);
+
+mySet.add(4); // add
+mySet.delete(4); // delete via index
+mySet.clear(); // delete all
+mySet.has(4); // like includes
+mySet.size; // length
+
+// =======================================================================
+let map = new Map();
+
+map.set("1", "str1"); // a string key
+map.set(1, "num1"); // a numeric key
+map.set(true, "bool1"); // a boolean key
+
+// remember the regular Object? it would convert keys to string
+// Map keeps the type, so these two are different:
+alert(map.get(1)); // 'num1'
+alert(map.get("1")); // 'str1'
+
+alert(map.size); // 3
+
+//
+// Map can also use objects as keys.
+
+let john = { name: "John" };
+
+// for every user, let's store their visits count
+let visitsCountMap = new Map();
+
+// john is the key for the map
+visitsCountMap.set(john, 123);
+
+alert(visitsCountMap.get(john)); // 123
+
+let john = { name: "John" };
+let ben = { name: "Ben" };
+
+let visitsCountObj = {}; // try to use an object
+
+visitsCountObj[ben] = 234; // try to use ben object as the key
+visitsCountObj[john] = 123; // try to use john object as the key, ben object will get replaced
+
+// That's what got written!
+alert(visitsCountObj["[object Object]"]); // 123
+
+// Chaining
+map.set("1", "str1").set(1, "num1").set(true, "bool1");
+
+// Iteration over Map
+// For looping over a map, there are 3 methods:
+
+// map.keys() – returns an iterable for keys,
+// map.values() – returns an iterable for values,
+// map.entries() – returns an iterable for entries [key, value], it’s used by default in for..of.
+
+let recipeMap = new Map([
+  ["cucumber", 500],
+  ["tomatoes", 350],
+  ["onion", 50],
+]);
+
+// iterate over keys (vegetables)
+for (let vegetable of recipeMap.keys()) {
+  alert(vegetable); // cucumber, tomatoes, onion
+}
+
+// iterate over values (amounts)
+for (let amount of recipeMap.values()) {
+  alert(amount); // 500, 350, 50
+}
+
+// iterate over [key, value] entries
+for (let entry of recipeMap) {
+  // the same as of recipeMap.entries()
+  alert(entry); // cucumber,500 (and so on)
+}
