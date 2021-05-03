967f0c1fcc6eebca68e516309a7380db74bacd66
Elcid Tesoro <elcid@innovationlove.ph>
2021-05-03 16:07:31 +0800

    Add get slug string in url

---
+++ slugifyUrl.js
@@ -0,0 +1,22 @@
+//  Get the slug string in url JavaScript
+// From https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1
+export default function slugifyUrl(string) {
+  if (string) {
+    const a = "àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;";
+    const b = "aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------";
+    const p = new RegExp(a.split("").join("|"), "g");
+
+    return string
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, "-") // Replace spaces with -
+      .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, "-and-") // Replace & with 'and'
+      .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+      .replace(/\-\-+/g, "-") // Replace multiple - with single -
+      .replace(/^-+/, "") // Trim - from start of text
+      .replace(/-+$/, ""); // Trim - from end of text
+  }
+
+  return null;
+}
